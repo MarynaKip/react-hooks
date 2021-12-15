4eb203d0462d887760564f885dd5ea7ac87870f6
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2021-12-15 19:08:49 +0200

    useState tutorial pushed to branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import { useForm } from "./useForm"
 
 function App() {
+  // const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+
+  // const [count, setCount] = useState(10);
+  // const [count2, setCount2] = useState(20);
+
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  const [values, handleChange] = useForm({ email: '', password: '' });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <button onClick={() =>
+        setCount(currentState => ({
+          ...currentState, count: currentState.count + 1
+        }))
+      }
+      >+</button> */}
+
+      {/* <button onClick={() =>
+        setCount(c => c + 1
+        )
+      }
+      >+</button>
+      <div>{count}</div>
+      <div>{count2}</div> */}
+
+      {/* <input
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      /> */}
+
+      <input
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
     </div>
   );
 }
